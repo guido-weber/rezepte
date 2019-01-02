9eb56083d8a4670c6ed36fae0342282b3c2a64e9
Guido Weber <guido.weber0815@googlemail.com>
2019-01-02 17:44:26 +0100

    Load receipes from BE on startup & render receipe list

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,6 +2,8 @@ import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, int, string, list, map3)
 import Url
 
 -- MAIN
@@ -19,20 +21,32 @@ main =
 
 -- MODEL
 
+type alias Receipe =
+    { id: Int
+    , name: String
+    , instructions: String
+    }
+
+type alias ReceipeList = List Receipe
+
+type Receipes = Loading | Success ReceipeList | Failure
+
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , receipes: Receipes
     }
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url, Cmd.none )
+    ( Model key url Loading, getReceipeList )
 
 -- UPDATE
 
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+    | GotReceipeList (Result Http.Error ReceipeList)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -50,6 +64,14 @@ update msg model =
             , Cmd.none
             )
 
+        GotReceipeList result ->
+            case result of
+                Ok receipes ->
+                    ({ model | receipes = Success receipes }, Cmd.none)
+
+                Err _ ->
+                    ({ model | receipes = Failure }, Cmd.none)
+
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
@@ -64,15 +86,47 @@ view model =
     , body =
         [ text "The current URL is: "
         , b [] [ text (Url.toString model.url) ]
-        , ul []
-            [ viewLink "/"
-            , viewLink "/rezepte/aaa"
-            , viewLink "/rezepte/bbb"
-            , viewLink "/rezepte/ccc"
-            ]
+        , viewReceipes model.receipes
         ]
     }
 
-viewLink : String -> Html msg
-viewLink path =
-    li [] [ a [ href path ] [ text path ] ]
+viewReceipe : Receipe -> Html msg
+viewReceipe receipe =
+    div [class "receipe"]
+        [ a [ href ("/rezepte/" ++ (String.fromInt receipe.id)) ] [ text receipe.name ]
+        , p [] [ text receipe.instructions ]
+        ]
+
+viewReceipes : Receipes -> Html Msg
+viewReceipes receipes =
+    case receipes of
+        Loading ->
+            text "Wait ..."
+
+        Success receipeList ->
+            div [class "receipe-list"]
+                (List.map viewReceipe receipeList)
+
+        Failure ->
+            text "Oops!"
+
+
+-- HTTP
+
+getReceipeList : Cmd Msg
+getReceipeList =
+    Http.get
+        { url = "/api/rezepte"
+        , expect = Http.expectJson GotReceipeList receipeListDecoder
+        }
+
+receipeDecoder : Decoder Receipe
+receipeDecoder =
+    map3 Receipe
+        (field "RezeptID" int)
+        (field "Bezeichnung" string)
+        (field "Anleitung" string)
+
+receipeListDecoder : Decoder ReceipeList
+receipeListDecoder =
+    list receipeDecoder
