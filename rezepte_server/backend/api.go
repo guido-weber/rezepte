9819f4671ab83c499b8b2b72c09fd0c6e9e28791
Guido Weber <guido.weber0815@googlemail.com>
2019-01-08 10:51:11 +0100

    Show all details (#12)
    * Zutaten auf Detailseite anzeigen
    
    * Tweak styles a bit

--- rezepte_server/backend/api.go
+++ rezepte_server/backend/api.go
@@ -21,10 +21,27 @@ type RezeptKopf struct {
 	Tags        []string `JSON:"Tags"`
 }
 
+// RezeptZutat bla
+type RezeptZutat struct {
+	RezeptZutatID int     `JSON:"RezeptZutatID"`
+	Zutat         string  `JSON:"Zutat"`
+	Menge         float64 `JSON:"Menge"`
+	Mengeneinheit string  `JSON:"Mengeneinheit"`
+	Bemerkung     string  `JSON:"Bemerkung"`
+}
+
+// RezeptTeil bla
+type RezeptTeil struct {
+	RezeptTeilID int           `JSON:"RezeptTeilID"`
+	Bezeichnung  string        `JSON:"Bezeichnung"`
+	Zutaten      []RezeptZutat `JSON:"Zutaten"`
+}
+
 // RezeptDetails is the full data
 type RezeptDetails struct {
 	RezeptKopf
-	Anleitung string `JSON:"Anleitung"`
+	Anleitung   string       `JSON:"Anleitung"`
+	RezeptTeile []RezeptTeil `JSON:"RezeptTeile"`
 }
 
 // RezepteHandler serves a list of RezeptKopf as JSON
@@ -104,18 +121,22 @@ func (hndlr RezepteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(rezepte)
 }
 
-const rezeptDetailsQuery = `SELECT r.rezept_id, bezeichnung, anleitung, GROUP_CONCAT(rt.tag) tags
+const rezeptDetailsQuery = `SELECT bezeichnung, anleitung, GROUP_CONCAT(rt.tag) tags
 	FROM tbl_rezepte r LEFT JOIN tbl_rezept_tags rt ON r.rezept_id = rt.rezept_id
 	WHERE r.rezept_id = ?
 	GROUP BY r.rezept_id, bezeichnung`
 
 func (hndlr RezeptDetailsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-	row := DB.QueryRow(rezeptDetailsQuery, key)
 	var rd RezeptDetails
+	vars := mux.Vars(r)
+	if key, err := strconv.Atoi(vars["key"]); err != nil {
+		log.Fatal(err)
+	} else {
+		rd.RezeptID = key
+	}
+	row := DB.QueryRow(rezeptDetailsQuery, rd.RezeptID)
 	var tags sql.NullString
-	if err := row.Scan(&rd.RezeptID, &rd.Bezeichnung, &rd.Anleitung, &tags); err != nil {
+	if err := row.Scan(&rd.Bezeichnung, &rd.Anleitung, &tags); err != nil {
 		log.Fatal(err)
 	}
 	err := rd.setLinks(hndlr.router)
@@ -123,6 +144,55 @@ func (hndlr RezeptDetailsHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 		log.Fatal(err)
 	}
 	rd.Tags = parseTags(tags)
+	if err := readZutaten(&rd); err != nil {
+		log.Fatal(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(rd)
+}
+
+const rezeptZutatenQuery = `SELECT
+		rt.rezept_teil_id, IFNULL(rt.bezeichnung, ''),
+		rz.rezept_zutat_id, rz.zutat, IFNULL(rz.menge, 0), IFNULL(rz.mengeneinheit, ''), IFNULL(rz.bemerkung, '')
+	FROM tbl_rezept_teile rt JOIN tbl_rezept_zutaten rz ON rt.rezept_teil_id = rz.rezept_teil_id
+	WHERE rt.rezept_id = ?
+	ORDER BY rt.reihenfolge, rz.reihenfolge`
+
+func readZutaten(rd *RezeptDetails) error {
+	rows, err := DB.Query(rezeptZutatenQuery, rd.RezeptID)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	teile := make([]RezeptTeil, 0)
+	zutaten := make([]RezeptZutat, 0)
+	var rt *RezeptTeil
+	for rows.Next() {
+		var rz RezeptZutat
+		var teilID int
+		var bezeichnung string
+		if err := rows.Scan(
+			&teilID, &bezeichnung,
+			&rz.RezeptZutatID, &rz.Zutat, &rz.Menge, &rz.Mengeneinheit, &rz.Bemerkung); err != nil {
+			return err
+		}
+		zutaten = append(zutaten, rz)
+		if rt == nil {
+			rt = &RezeptTeil{RezeptTeilID: teilID, Bezeichnung: bezeichnung}
+		} else if teilID != rt.RezeptTeilID {
+			rt.Zutaten = zutaten
+			teile = append(teile, *rt)
+			rt = &RezeptTeil{RezeptTeilID: teilID, Bezeichnung: bezeichnung}
+			zutaten = make([]RezeptZutat, 0)
+		}
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+	if rt != nil {
+		rt.Zutaten = zutaten
+		teile = append(teile, *rt)
+	}
+	rd.RezeptTeile = teile
+	return nil
 }
